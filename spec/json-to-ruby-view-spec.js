d48d4b7e09e30df8626cacc429a5d4c448e96bee
jbjbhatcher <jbhtcher@memphis.edu>
2019-12-20 22:39:36 -0600

    Remove view spec

--- spec/json-to-ruby-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import JsonToRubyView from '../lib/json-to-ruby-view';
-
-describe('JsonToRubyView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
